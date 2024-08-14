72f7b4d56b8a87a075d48fbdbb085d7e52708955
bradkerwin <bkerwin101@gmail.com>
2024-08-14 13:08:54 -0400

    initial commit

---
+++ monday-react-homework/src/CodeCorrection.jsx
@@ -0,0 +1,53 @@
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: 'Alex' };
+    }
+
+    // Incorrect function
+//     const changeName = () => {
+//         setName('Charlie');
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>User Profile</h1>
+//                 <p>Name: {this.state.name}</p>
+//                 <button onClick={this.changeName}>Change Name</button>
+//             </div>
+//         );
+//     }
+// }
+
+// export default changeName();
+
+// corrected function
+import { Component } from "react";
+
+class UserProfile extends Component {
+    constructor() {
+        super();
+        this.state = { name: 'Alex' };
+        this.changeName =  this.changeName.bind(this)
+    }
+
+    // Incorrect function
+    changeName() {
+        this.setState({name: 'Charlie'});
+    }
+
+    render() {
+        const name = this.state.name;
+        return (
+            <div>
+                <h1>User Profile</h1>
+                <p>Name: {name}</p>
+                <button onClick={this.changeName}>Change Name</button>
+            </div>
+        )
+    }
+}
+
+
+export default UserProfile
---
+++ monday-react-homework/src/EnhancingFunctionalComponentsWithReactHooks.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const moviesList = () => {
+    const movies [movies, setMovies] = useState([
+        "The Sandlot", "Pulp Fiction", "Soul Surfer", 
+    "Saving Private Ryan", "Black Mass", "Slapshot", "Friday Night Lights"
+    ])
+}
+
+const movieSummaries = () => {
+    const summaries [summaries, setSummaries] = useState([
+        "In the summer of 1962, a new kid in town is taken under the wing of a young baseball prodigy and his rowdy team, resulting in many adventures.",
+        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        "Bethany Hamilton, a professional surfer, loses her left arm in a shark attack. Unwilling to pay attention to the gravity of her situation, Bethany decides to get back into the ocean and surf again.",
+        "Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
+        "The true story of Whitey Bulger, the brother of a state senator and the most infamous violent criminal in the history of South Boston, who became an FBI informant to take down a Mafia family invading his turf.",
+        "A failing ice hockey team finds success with outrageously violent hockey goonery.",
+        "Based on H.G. Bissinger's book, which profiled the economically depressed town of Odessa, Texas and their heroic high school football team, The Permian High Panthers."
+    ])
+}
+
+export default moviesList;
